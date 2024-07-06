b743b99d3dc6950b32699aead1b31174c914a800
Cartunduaga123 <52115389+Cartunduaga123@users.noreply.github.com>
2024-07-05 23:31:16 -0500

    enable

--- src/main/java/com/unir/movie_app_search/MovieAppSearchApplication.java
+++ src/main/java/com/unir/movie_app_search/MovieAppSearchApplication.java
@@ -3,9 +3,11 @@ package com.unir.movie_app_search;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @Slf4j
+@EnableEurekaClient
 public class MovieAppSearchApplication {
 
 
