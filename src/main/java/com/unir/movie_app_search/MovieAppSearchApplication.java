d589c4b20efc5956b8bc0f032479ac9aa519fc12
Cartunduaga123 <52115389+Cartunduaga123@users.noreply.github.com>
2024-07-05 23:11:23 -0500

    movie search

--- src/main/java/com/unir/movie_app_search/MovieAppSearchApplication.java
+++ src/main/java/com/unir/movie_app_search/MovieAppSearchApplication.java
@@ -1,12 +1,25 @@
 package com.unir.movie_app_search;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class MovieAppSearchApplication {
 
+
 	public static void main(String[] args) {
+		// Obtenemos perfil de ejecucion de variable de entorno. Si no hay, perfil default.
+		String profile = System.getenv("PROFILE");
+		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
+
+		log.debug("Waiting fo Internal Interface to start...");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		SpringApplication.run(MovieAppSearchApplication.class, args);
 	}
 
